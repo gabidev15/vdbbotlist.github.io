0ec2bfdb07550cd580e4ea1876438e49885f6ae0
gabidev15 <59804519+gabidev15@users.noreply.github.com>
2020-07-25 17:09:20 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    port: 80,
+
+    language: 'en-us',
+
+    url_link: 'http://localhost',
+
+    discord: {
+        client_id: '653661971178061845',
+        client_secret: '5LkMAvXper62Y0XYwwtHFhugdliXDKeE',
+        token: 'NjUzNjYxOTcxMTc4MDYxODQ1.Xe6QJg.Fhn9b-DZeOeCA3OMz1oFxULxNmQ',
+        notification: true,
+        guild_id: '637918501545443359',
+        mod_role_id: '638423241811296290',
+        admin_role_id: '637919241181462528',
+        BOT_ROLE_ID: '637993004279398418',
+        UNVERIFIED_ROLE_ID: '638423241811296290',
+        ADMIN_USER: '638423241811296290',
+        log_id: '646029203954991104',
+        mod_log_id: '646029203954991104',
+        VERIFY_ROLES_ID: '637919241181462528',
+        UNMUTED_ROLES_ID: '727059935694684181',
+        BOT_DEV_ROLE: '638423241811296290',
+        GUILD_INVITE: 'https://discord.gg/CprdTD'
+    },
+
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'botlist',
+        MONGO_DB_URL: 'mongodb://localhost:27017/mydb'
+    },
+
+    bot: {
+        owner: 'DEV Gabi#6253',
+        co_owner: 'DEV Gabi#6253',
+        prefix: 'm!'
+    }
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+require('dotenv').config();
+//require("module-alias/register");
+const config = require('./config');
+var fs = require('fs');
+const mongoose = require('mongoose');
+var express = require('express');
+var createError = require('http-error');
+var app = express();
+const colors = require('colors');
+const App = require('./structures/app.js');
+const bot = require('./client/bot');
+const {MONGO_DB_URL} = process.env;
+
+  let botname = "Muge";
+
+    //CONNECT TO THE DATABASE
+    mongoose.connect(`${config.database.MONGO_DB_URL}`, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(colors.yellow(`Connected to the database on `) + colors.underline.green(MONGO_DB_URL));
+    //Logged In The Bot
+    let client = bot.init(config.discord.token);
+    console.log(colors.yellow(`Logged in as `) + colors.underline.green(botname));
+    //Listen To The Server Port
+     new App(client).listen(config.port || 8080);
+    console.log(colors.yellow(`Running on port `) + colors.underline.green(config.port || 8080));
